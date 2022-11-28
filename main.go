36b074fe896fd8c4b977189568f5ba66fdd8008a
Talha Yazar <talhayazarg@gmail.com>
2022-11-28 22:50:21 +0300

    We created our routes for lead requests
    We paid attention to the rest structure

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"github.com/gofiber/fiber"
 	"github.com/talha-yazar/Go-Fiber-CRM-Basic/database"
+	"github.com/talha-yazar/Go-Fiber-CRM-Basic/lead"
 )
+
+// setupRoutes is a function that sets up all the routes for the application
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Put("/api/v1/lead/:id", lead.UpdateLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
 
 func main() {
 	app := fiber.New()
+	setupRoutes(app)
 	app.Listen(3000)
 	defer database.DBConn.Close()
 }
